815022cd0335a9cea6ce484244c52b51fefb43d9
code-for-yokohama <code.for.yokohama@gmail.com>
2020-04-24 18:54:24 +0900

    注釈削除

--- components/cards/ConfirmedCasesDetailsCard.vue
+++ components/cards/ConfirmedCasesDetailsCard.vue
@@ -5,27 +5,6 @@
       :title-id="'details-of-confirmed-cases'"
       :date="updatedAt"
     >
-      <template v-slot:description>
-        <ul>
-          <li>
-            {{
-              $t('（注）チャーター機帰国者、クルーズ船乗客等は含まれていない')
-            }}
-          </li>
-          <li>
-            {{
-              $t('（注）「入院中」には、入院調整中・宿泊療養に移行した方を含む')
-            }}
-          </li>
-          <li>
-            {{
-              $t(
-                '（注）退院者数の把握には一定の期間を要しており、確認次第数値を更新している'
-              )
-            }}
-          </li>
-        </ul>
-      </template>
       <confirmed-cases-details-table
         :aria-label="$t('検査陽性者の状況')"
         v-bind="confirmedCases"

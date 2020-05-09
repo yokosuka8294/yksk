ef245733ea6936a0f7f6c6d101210659145dabe1
covid19yokohama <covid19yokohama@gmail.com>
2020-05-10 06:52:03 +0900

    auto update

---
+++ data/git_auto_push_to_master.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# update json. if update make update.txt
+today=$(date "+%Y%m%d");
+echo "${today} php update...";
+
+#php update.php
+
+# if update.txt exist
+#if [[ -f ./update.txt ]]; then
+
+	echo 'git pull';
+	git pull;
+	echo '';
+
+	echo 'git add .';
+	git add .;
+	echo '';
+
+	echo 'git tag -d ';
+	git tag -d master-2020-02-04;
+	git tag -d refs/heads/master;
+	echo '';
+
+	echo 'git merge -m ';
+	git commit -m "auto update";
+	echo '';
+
+	echo 'git push';
+	git push;
+	echo '';
+
+	echo "wait 5min for tweet...";
+	sleep 300;
+
+#	php tweet.php
+
+#	rm update.txt
+#fi

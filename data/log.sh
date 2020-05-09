272c06cd4aaf8703f887b045de81e8bf03a7cbaa
covid19yokohama <covid19yokohama@gmail.com>
2020-05-09 21:30:04 +0900

    auto update

---
+++ data/log.sh
@@ -0,0 +1,28 @@
+php update...
+no update
+done.
+php update...
+no update
+done.
+php update...
+no update
+done.
+php update...
+no update
+done.
+php update...
+no update
+done.
+php update...
+1 update positive patioen num: data.json, agency2.json
+git update...
+git checkout development
+M	data/agency2.json
+M	data/agency3.json
+M	data/data.json
+Your branch is up-to-date with 'origin/development'.
+
+git pull
+Already up-to-date.
+
+git add .
